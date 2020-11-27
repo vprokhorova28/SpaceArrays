86384386cca7e626ee5fc313095a52c254bc839f
vprokhorova28 <vpro2004@mail.ru>
2020-11-27 11:06:02 +0300

    Version 2.0

--- app/src/main/java/ru/samsung/itschool/spacearrays/Rocket.java
+++ app/src/main/java/ru/samsung/itschool/spacearrays/Rocket.java
@@ -5,31 +5,67 @@ import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 
-public class Rocket {
-    float x, y, vx, vy;
-    Bitmap image;
-    void move(){
-        x += vx;
-        y += vy;
-    }
+public class Rocket extends BaseObject implements Moveable, Touchable{
+    private float vx, vy;
+    private Bitmap image;
+    private int w, h;
 
-    Rocket(float x, float y, float vx, float vy, Bitmap image) {
-        this.x = x;
-        this.y = y;
+    Rocket(float x, float y, float vx, float vy, Bitmap image, int w, int h) {
+        super(x, y);
+        this.w = w;
+        this.h = h;
         this.vx = vx;
         this.vy = vy;
         this.image = image;
     }
 
+    @Override
+    public void move(){
+        double x = getX();
+        double  y = getY();
+        setX(x + vx);
+        setY(y + vy);
+    }
 
-    void draw(Canvas canvas, Paint paint)
-    {
+    @Override
+    public void draw(Canvas canvas) {
+        Paint paint = new Paint();
         Matrix matrix = new Matrix();
         matrix.setScale(0.2f, 0.2f);
         //Study mathematics, dear young programmer :)
+        /*if (getX() < 30) {
+            vx *= -1;
+
+            double y = getY();
+            setX(y - 120);
+        }
+        if (getX() > w - 30) {
+            vx *= -1;
+            double y = getY();
+            setX(y - 120);
+        }
+        if (getY() < 30){
+            vy *= -1;
+            double x = getX();
+            setX(x + 120);
+        }
+        if (getY() > h - 30){
+            vy *= -1;
+            double x = getX();
+            setX(x - 120);
+        }*/
+
         matrix.postRotate((float)Math.toDegrees(Math.atan2(vy, vx)) + 45);
-        matrix.postTranslate(x, y);
+        matrix.postTranslate((float)getX(), (float)getY());
         paint.setAlpha(255);
         canvas.drawBitmap(image, matrix, paint);
+    }
+
+    @Override
+    public void touch(float x, float y) {
+        if (Math.abs(getX() - x ) <= 100 && Math.abs(getY() - y) <= 100) {
+            setY(y + 120);
+            setX(x - 120);
+        }
     }
 }
--- app/src/main/java/ru/samsung/itschool/spacearrays/Sky.java
+++ app/src/main/java/ru/samsung/itschool/spacearrays/Sky.java
@@ -4,39 +4,27 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 
-public class Sky {
-    int numStars;
-    int xStar[];
-    int yStar[];
-    int alphaStar[];
-    Sky() {
-        this.numStars = 500;
-        this.xStar = new int[numStars];
-        this.yStar = new int[numStars];
-        this.alphaStar = new int[numStars];
-        int maxX = 2000;
-        int maxY = 2000;
-        for (int i = 0; i < numStars; i++)
+public class Sky implements Drawable{
+    private Star[] stars;
+
+    Sky(int w, int h, int n) {
+        int maxX = w;
+        int maxY = h;
+        stars = new Star[n];
+        for (int i = 0; i < stars.length; i++)
         {
-            xStar[i] = (int)(Math.random() * maxX);
-            yStar[i] = (int)(Math.random() * maxY);
-            alphaStar[i] = (int)(Math.random() * 256);
+            stars[i] = new Star(Math.random() * maxX,
+                    Math.random() * maxY,
+                    (int)(Math.random() * 256));
         }
     }
 
-
-    void draw(Canvas canvas) {
+    @Override
+    public void draw(Canvas canvas) {
         Paint paint = new Paint();
         canvas.drawColor(Color.BLACK);
-        paint.setColor(Color.YELLOW);
-        paint.setStrokeWidth(2);
-        for (int i = 0; i < numStars; i++)
-        {
-            paint.setAlpha(alphaStar[i]);
-            alphaStar[i] += (int)(Math.random() * 11) - 5;
-            if (alphaStar[i] > 255) alphaStar[i] = 255;
-            if (alphaStar[i] < 0) alphaStar[i] = 0;
-            canvas.drawCircle(xStar[i], yStar[i], 3, paint);
+        for (Star star : stars) {
+            star.draw(canvas);
         }
     }
 }
